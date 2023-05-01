7f7cdffedca523f0abce6fba5cf4744a5dd427f8
Pasca1Etienne <132153686+Pasca1Etienne@users.noreply.github.com>
2023-05-01 09:41:18 +0200

    Uses timezone mapper

--- RPCServer/Services/TimeZoneService.cs
+++ RPCServer/Services/TimeZoneService.cs
@@ -1,13 +1,22 @@
 ﻿using Grpc.Core;
 using RPCServer;
+using RPCServer.Models.TimeZoneMapper;
 
 namespace RPCServer.Services
 {
     public class TimeZoneService : Clock.ClockBase
     {
+        private ITimeZoneMapper _timezoneMapper;
+
+        public TimeZoneService(ITimeZoneMapper timezoneMapper)
+        {
+            _timezoneMapper = timezoneMapper;
+        }
+
         public override Task<TimeReply> GetTime(TimeRequest request, ServerCallContext context)
         {
-            var currentTime = DateTime.UtcNow;
+            var timeShift = _timezoneMapper.GetTimeDifference(request.Timezone);
+            var currentTime = DateTime.UtcNow.AddHours(timeShift);
             return Task.FromResult<TimeReply>(new TimeReply()
             {
                 Message = currentTime.Ticks
