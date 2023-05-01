2cbbaecfed647abf54e6f640cb1783277d7041be
Pasca1Etienne <132153686+Pasca1Etienne@users.noreply.github.com>
2023-05-01 09:04:40 +0200

    Initial TimeZone Service

---
+++ RPCServer/Services/TimeZoneService.cs
@@ -0,0 +1,17 @@
+﻿using Grpc.Core;
+using RPCServer;
+
+namespace RPCServer.Services
+{
+    public class TimeZoneService
+    {
+        public Task<TimeReply> GetTime(TimeRequest request, ServerCallContext context)
+        {
+            var currentTime = DateTime.UtcNow;
+            return Task.FromResult<TimeReply>(new TimeReply()
+            {
+                Message = currentTime.Ticks
+            });
+        }
+    }
+}
